49d92c8785c1936e88b68ec93076cac765179adc
cite <cite@berlin.com>
2017-11-30 03:09:46 +0100

    hands

--- bare_metal_provisioning.sh
+++ bare_metal_provisioning.sh
@@ -2,4 +2,22 @@
 
 # execute this file to prepare the host which runs the vhosts later
 
+# install vagrant
+sudo apt-get update
 sudo apt-get install vagrant
+
+# add backports-repo, needed vor virtualbox
+echo "deb http://ftp.debian.org/debian stretch-backports main" | sudo tee /etc/apt/sources.list.d/backports.list > /dev/null
+
+# add virtualbox-repo
+echo "deb http://download.virtualbox.org/virtualbox/debian stretch contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list > /dev/null
+
+# add certificate
+curl -O https://www.virtualbox.org/download/oracle_vbox_2016.asc
+sudo apt-key add oracle_vbox_2016.asc
+
+# install virtualbox
+sudo apt-get update
+sudo apt-get install virtualbox-5.1
+
+
---
+++ vms/swarm_managers/docker_provisioning.sh
@@ -0,0 +1,12 @@
+sudo yum update -y
+sudo yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+sudo yum-config-manager \
+  --add-repo \
+  https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install -y docker-ce
+sudo systemctl enable docker
+sudo usermod -aG docker vagrant
+sudo systemctl start docker
+
