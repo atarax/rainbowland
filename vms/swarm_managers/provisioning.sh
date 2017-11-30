59d8b72d5b9e4db5a7d0c5a12db3502b7b3e585a
cite <cite@berlin.com>
2017-11-30 20:57:31 +0100

    hands

---
+++ vms/swarm_managers/provisioning.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-get update -y
+sudo apt-get install -y \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+sudo apt-get update
+
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get update
+sudo apt-get install -y docker-ce
+
+sudo groupadd docker
+sudo usermod -aG docker vagrant
+
+#update-rc.d docker enable
+#service docker start
